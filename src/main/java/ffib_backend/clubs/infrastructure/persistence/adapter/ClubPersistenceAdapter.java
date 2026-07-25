a4cbbe09b72bacf1d4be32b5ac46439d2753375e
Javier García Hernández <147824661+jgarciahweb@users.noreply.github.com>
2026-07-25 17:34:03 +0200

    [FFIB-14] feat: configure PostgreSQL connectivity (#3)

---
+++ src/main/java/ffib_backend/clubs/infrastructure/persistence/adapter/ClubPersistenceAdapter.java
@@ -0,0 +1,10 @@
+package ffib_backend.clubs.infrastructure.persistence.adapter;
+
+import ffib_backend.clubs.application.port.out.ClubPersistencePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ClubPersistenceAdapter implements ClubPersistencePort {
+}
